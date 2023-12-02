6b68705b224f7a0407caaef5cf30f95e433e53b0
ashiruhammed <hammedashiru89@gmail.com>
2023-12-02 18:03:57 +0100

    added-update

---
+++ index.js
@@ -0,0 +1,101 @@
+// TODO: write `findAll(..)`
+
+// tests:
+var myObj = { a: 2 };
+
+var values = [
+  null,
+  undefined,
+  -0,
+  0,
+  13,
+  42,
+  NaN,
+  -Infinity,
+  Infinity,
+  "",
+  "0",
+  "42",
+  "42hello",
+  "true",
+  "NaN",
+  true,
+  false,
+  myObj,
+];
+
+function findAll(checkValue, ArrVal) {
+  const value = ArrVal.filter((val) => {
+    const stringNumber =
+      typeof checkValue == "string" &&
+      Boolean(checkValue) &&
+      typeof val == "number";
+    const numberString =
+      typeof val == "string" && Boolean(val) && typeof checkValue == "number";
+
+    if (stringNumber || numberString) {
+      return checkValue == val && isNegative(val) == isNegative(checkValue);
+    }
+
+    if (
+      (checkValue == null && val == undefined) ||
+      (val == null && checkValue == undefined)
+    )
+      return true;
+    else return Object.is(checkValue, val);
+  });
+
+  console.log(value);
+}
+
+function isNegative(x) {
+  return 1 / x == -Infinity;
+}
+
+console.log(setsMatch(findAll(null, values)));
+console.log(setsMatch(findAll(undefined, values), [null, undefined]) === true);
+console.log(setsMatch(findAll(0, values), [0, "0"]));
+console.log(setsMatch(findAll(-0, values), [-0]) === true);
+console.log(setsMatch(findAll(13, values), [13]) === true);
+console.log(setsMatch(findAll(42, values), [42, "42"]) === true);
+console.log(setsMatch(findAll(NaN, values), [NaN]) === true);
+console.log(setsMatch(findAll(-Infinity, values), [-Infinity]) === true);
+console.log(setsMatch(findAll(Infinity, values), [Infinity]) === true);
+console.log(setsMatch(findAll("", values), [""]));
+console.log(setsMatch(findAll("0", values), [0, "0"]) === true);
+console.log(setsMatch(findAll("42", values), [42, "42"]) === true);
+console.log(setsMatch(findAll("42hello", values), ["42hello"]) === true);
+console.log(setsMatch(findAll("true", values), ["true"]) === true);
+console.log(setsMatch(findAll(true, values), [true]) === true);
+console.log(setsMatch(findAll(false, values), [false]) === true);
+console.log(setsMatch(findAll(myObj, values), [myObj]) === true);
+
+console.log(setsMatch(findAll(null, values), [null, 0]) === false);
+console.log(setsMatch(findAll(undefined, values), [NaN, 0]) === false);
+console.log(setsMatch(findAll(0, values), [0, -0]) === false);
+console.log(setsMatch(findAll(42, values), [42, "42hello"]) === false);
+console.log(setsMatch(findAll(25, values), [25]) === false);
+console.log(
+  setsMatch(findAll(Infinity, values), [Infinity, -Infinity]) === false
+);
+console.log(setsMatch(findAll("", values), ["", 0]) === false);
+console.log(setsMatch(findAll("false", values), [false]) === false);
+console.log(setsMatch(findAll(true, values), [true, "true"]) === false);
+console.log(setsMatch(findAll(true, values), [true, 1]) === false);
+console.log(setsMatch(findAll(false, values), [false, 0]) === false);
+
+// ***************************
+
+function setsMatch(arr1, arr2) {
+  if (
+    Array.isArray(arr1) &&
+    Array.isArray(arr2) &&
+    arr1.length == arr2.length
+  ) {
+    for (let v of arr1) {
+      if (!arr2.includes(v)) return false;
+    }
+    return true;
+  }
+  return false;
+}
